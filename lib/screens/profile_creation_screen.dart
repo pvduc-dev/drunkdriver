3c112aa58d0abd736b5aa2f81b476bb7d57d3c15
pvduc <pvduc.dev@gmail.com>
2025-03-23 01:17:46 +0700

    Refactor form implementation and update dependencies
    Replaced `flutter_form_builder` and related packages with custom widgets for enhanced control and simplified functionality. Updated `pubspec.yaml` with necessary dependency adjustments, and updated `pubspec.lock` to reflect the dependency changes.

--- lib/screens/profile_creation_screen.dart
+++ lib/screens/profile_creation_screen.dart
@@ -2,9 +2,6 @@ import 'package:drunkdriver/widgets/primary_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:form_builder_validators/form_builder_validators.dart';
-import 'package:form_builder_extra_fields/form_builder_extra_fields.dart';
 
 class ProfileCreationScreen extends StatefulWidget {
   const ProfileCreationScreen({super.key});
@@ -14,7 +11,8 @@ class ProfileCreationScreen extends StatefulWidget {
 }
 
 class _ProfileCreationScreenState extends State<ProfileCreationScreen> {
-  final _formKey = GlobalKey<FormBuilderState>();
+  String selectedGender = 'Male';
+  DateTime? _selectedDate;
 
   @override
   Widget build(BuildContext context) {
@@ -23,177 +21,167 @@ class _ProfileCreationScreenState extends State<ProfileCreationScreen> {
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: FormBuilder(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(height: 72),
-                Column(
-                  children: [
-                    Column(
+          child: Column(
+            children: [
+              SizedBox(height: 72),
+              Column(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Họ và tên',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      CupertinoTextField(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFE2EAF1).withAlpha(51),
+                          border: Border.all(
+                            color: Color(0xFF8FA1B7).withAlpha(89),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        onChanged: (value) {
+                          // Handle changes here
+                        },
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Họ và tên',
+                          'Giới tính',
                           style: TextStyle(
                             fontSize: 16.0,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
                         SizedBox(height: 8.0),
-                        FormBuilderTextField(
-                          name: 'fullName',
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Color(0xFFE2EAF1).withAlpha(51),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(
-                                color: Color(0xFF8FA1B7).withAlpha(89),
-                                width: 1,
-                              ),
+                        Row(
+                          children: [
+                            Radio<String>(
+                              value: 'Male',
+                              groupValue: selectedGender,
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedGender = value!;
+                                });
+                              },
                             ),
-                            contentPadding: EdgeInsets.all(16.0),
-                          ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Vui lòng nhập họ tên';
-                            }
-                            return null;
-                          },
+                            Text('Nam', style: TextStyle(fontSize: 16.0)),
+                            SizedBox(width: 16),
+                            Radio<String>(
+                              value: 'Female',
+                              groupValue: selectedGender,
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedGender = value!;
+                                });
+                              },
+                            ),
+                            Text('Nữ', style: TextStyle(fontSize: 16.0)),
+                          ],
                         ),
                       ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Giới tính',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(height: 8.0),
-                          FormBuilderRadioGroup(
-                            name: 'gender',
-                            options: [
-                              FormBuilderFieldOption(
-                                value: 'Male',
-                                child: Text('Nam'),
-                              ),
-                              FormBuilderFieldOption(
-                                value: 'Female',
-                                child: Text('Nữ'),
-                              ),
-                            ],
-                            initialValue: 'Male',
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.zero,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Ngày sinh',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(height: 8.0),
-                          FormBuilderDateTimePicker(
-                            name: 'birthDate',
-                            inputType: InputType.date,
-                            format: DateFormat('dd/MM/yyyy'),
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Color(0xFFE2EAF1).withAlpha(51),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                  color: Color(0xFF8FA1B7).withAlpha(89),
-                                  width: 1,
-                                ),
-                              ),
-                              prefixIcon: Icon(CupertinoIcons.calendar),
-                              contentPadding: EdgeInsets.all(16.0),
-                            ),
-                            validator: (value) {
-                              if (value == null) {
-                                return 'Vui lòng chọn ngày sinh';
-                              }
-                              return null;
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Địa chỉ nhà',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(height: 8.0),
-                          FormBuilderTextField(
-                            name: 'address',
-                            maxLines: 3,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Color(0xFFE2EAF1).withAlpha(51),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                  color: Color(0xFF8FA1B7).withAlpha(89),
-                                  width: 1,
-                                ),
-                              ),
-                              contentPadding: EdgeInsets.all(16.0),
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Vui lòng nhập địa chỉ';
-                              }
-                              return null;
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Expanded(child: Container()),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0),
-                  child: PrimaryButton(
-                    onPressed: () {
-                      if (_formKey.currentState?.saveAndValidate() ?? false) {
-                        final formData = _formKey.currentState!.value;
-                        print(formData);
-                        // TODO: Handle form submission
-                      }
-                    },
-                    text: 'Đăng ký',
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ngày sinh',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                        GestureDetector(
+                          onTap: () async {},
+                          child: Container(
+                            padding: const EdgeInsets.all(12.0),
+                            decoration: BoxDecoration(
+                              color: Color(0xFFE2EAF1).withAlpha(51),
+                              border: Border.all(
+                                color: Color(0xFF8FA1B7).withAlpha(89),
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(CupertinoIcons.calendar),
+                                SizedBox(width: 8.0),
+                                Text(
+                                  _selectedDate != null
+                                      ? DateFormat(
+                                        'dd/MM/yyyy',
+                                      ).format(_selectedDate!)
+                                      : 'Chọn ngày sinh',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: CupertinoColors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Địa chỉ nhà',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                        CupertinoTextField(
+                          padding: const EdgeInsets.all(16.0),
+                          maxLines: 3,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFE2EAF1).withAlpha(51),
+                            border: Border.all(
+                              color: Color(0xFF8FA1B7).withAlpha(89),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(child: Container()),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: PrimaryButton(
+                  onPressed: () {
+                    // Add your action here
+                  },
+                  text: 'Đăng ký',
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
